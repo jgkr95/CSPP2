a3ad8c39de7e34fbc65b936d27dcfd382147f96b
jgkr95 <jgkr95@msitprogram.net>
2018-08-27 20:17:39 +0530

    Helloworld first practice program

---
+++ cspp2-practice/M1/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+* First program of java.
+*/
+public class HelloWorld
+{
+	/**
+	* @param args main class.
+	*/
+	public static void main(String[] args)
+	{
+		/**
+		*main method.
+		*/
+		System.out.println("Hello World.......");
+	}
+}
