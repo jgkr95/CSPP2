76c54307642dd528b0aaf2d430acdd6c06a190e6
jgkr95 <jgkr95@msitprogram.net>
2018-08-28 13:49:00 +0530

    Printing name and roll number

---
+++ cspp2-assignments/M1/Assignment-1/Solution.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Solution {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String name = s.next();
+		int roll_number = s.nextInt();
+		System.out.println("Name : "+name);
+		System.out.println("Roll Number: "+roll_number);
+	}
+}
