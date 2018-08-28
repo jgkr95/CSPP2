235fa8bd7123b40d7eaba2a7c9ccb045a1b39aec
jgkr95 <jgkr95@msitprogram.net>
2018-08-28 17:43:04 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/M2/Assignment-3/Solution.java
+++ cspp2-assignments/M2/Assignment-3/Solution.java
@@ -3,44 +3,44 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * { var_description }.
-	 */
-	private Solution() {
-		/**
-		 * { item_description }.
-		 */
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);      
+    /**
+     * { var_description }.
+     */
+    private Solution() {
+        /**
+         * { item_description }.
+         */
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
         int base = s.nextInt();
         int exponent = s.nextInt();
         long result = power(base, exponent);
         System.out.println(result);
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      b     { parameter_description }
-	 * @param      e     { parameter_description }
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public static long power(final int b, final int e) {
-		/**
-		 * { item_description }.
-		 */
-		if (e == 0) {
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      b     { parameter_description }
+     * @param      e     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static long power(final int b, final int e) {
+        /**
+         * { item_description }.
+         */
+        if (e == 0) {
 
-			return 1;
-		}
-		else {
-			return b * power(b, e - 1);
-		}
-	}
+            return 1;
+        }
+        else {
+            return b * power(b, e - 1);
+        }
+    }
 }
