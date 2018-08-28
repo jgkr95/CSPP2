eaaf6dea3849096d40c099c8eb00c8fd1a5bd217
jgkr95 <jgkr95@msitprogram.net>
2018-08-28 14:29:50 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/M2/Assignment-3/Solution.java
+++ cspp2-assignments/M2/Assignment-3/Solution.java
@@ -5,10 +5,10 @@ public class Solution {
 	*/
 	public static int res = 1;
 	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);      
+		Scanner s = new Scanner(System.in);      
         int base = s.nextInt();
         int exponent = s.nextInt();
-        long result=power(base,exponent);
+        long result = power(base, exponent);
         System.out.println(result);
 	}
 	/*
@@ -16,9 +16,9 @@ public class Solution {
 	*/
 	public static long power(int b, int e) {
 		// int res = ;
-		if(e==0)
+		if(e == 0)
 			return 1;
 		else
-			return b*power(b,e-1);
+			return b * power(b, e-1);
 	}
 }
