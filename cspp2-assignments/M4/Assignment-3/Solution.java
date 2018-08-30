842ab065fa39ef6cef085b63a7038d1e169cc4ef
jgkr95 <jgkr95@msitprogram.net>
2018-08-30 15:01:50 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2-assignments/M4/Assignment-3/Solution.java
+++ cspp2-assignments/M4/Assignment-3/Solution.java
@@ -1,23 +1,19 @@
 
-// Given an String, Write a java method that returns the 
+// Given an String, Write a java method that returns the
 //decimal value for the given binary string.
 import java.util.Scanner;
 /**
  * Class for solution.
- * // Given an String, Write a java method that returns the 
+ * // Given an String, Write a java method that returns the
  *decimal value for the given binary string.
  */
-public class Solution
-{/*
-    Do not modify this main function.
-    */
-
+public final class Solution {
     /**
      * { function_description }.
      *
      * @param      args  The arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.nextLine();
