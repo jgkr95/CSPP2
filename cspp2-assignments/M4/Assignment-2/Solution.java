76b812ce45160d1c07e63de96d7cf47d1687c7f7
jgkr95 <jgkr95@msitprogram.net>
2018-08-30 13:54:26 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

---
+++ cspp2-assignments/M4/Assignment-2/Solution.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public class Solution {
+    /* Fill the main function to print resultant of addition of matrices*/
+    /**
+     * Constructs the object.
+     */
+    private Solution() { }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        int m = s.nextInt();
+        int n = s.nextInt();
+        int[][] a = new int[m][n];
+        int[][] c = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = s.nextInt();
+            }
+        }
+        int p = s.nextInt();
+        int q = s.nextInt();
+        int[][] b = new int[m][n];
+        for (int i = 0; i < p; i++) {
+            for (int j = 0; j < q; j++) {
+                b[i][j] = s.nextInt();
+            }
+        }
+        if (m == p && n == q) {
+            for (int i = 0; i < p; i++) {
+            for (int j = 0; j < q; j++) {
+                c[i][j] = a[i][j] + b[i][j];
+                System.out.print(c[i][j]+" ");
+            }
+            System.out.println();
+            }
+        }
+
+        else {
+            System.out.println("not possible");
+        
+        }
+    }
+}
