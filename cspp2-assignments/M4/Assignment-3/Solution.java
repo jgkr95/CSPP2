2a134e9bf49ce5e20932c51ddcc8325b644abcda
jgkr95 <jgkr95@msitprogram.net>
2018-08-30 15:02:35 +0530

    "check_style_score -> 1 of 1 passed. style score: 1/1 "

--- cspp2-assignments/M4/Assignment-3/Solution.java
+++ cspp2-assignments/M4/Assignment-3/Solution.java
@@ -3,11 +3,14 @@
 //decimal value for the given binary string.
 import java.util.Scanner;
 /**
- * Class for solution.
- * // Given an String, Write a java method that returns the
- *decimal value for the given binary string.
+ * Class for solution. // Given an String, Write a java method that returns the
+ * decimal value for the given binary string.
  */
 public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() { }
     /**
      * { function_description }.
      *
