74220cfb410aad5709c2a597cadf186e7abeb973
jgkr95 <jgkr95@msitprogram.net>
2018-08-31 18:26:04 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/M2/Assignment-2/Solution.java
+++ cspp2-assignments/M2/Assignment-2/Solution.java
@@ -6,14 +6,12 @@ public final class Solution {
     /**
      * Constructs the object.
      */
-    private Solution() {
+    private Solution() { }
     /**
-     * { item_description }.
+     * { this is main method and it reads inputs for finiding quadratic equation }
+     *
+     * @param      args  The arguments are final here
      */
-    }
-    /**
-        * @param args Do not modify this main function.
-    */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
@@ -22,11 +20,11 @@ public final class Solution {
         rootsOfQuadrati(a, b, c);
     }
     /**
-     * { function_description }.
+     * { This is the method for finding the roots of the quadratic equation }.
      *
-     * @param      a     { parameter_description }
-     * @param      b     { parameter_description }
-     * @param      c     { parameter_description }
+     * @param      a     { integer type varible }
+     * @param      b     { integer type varible }
+     * @param      c     { integer type variable }
      */
     public static void rootsOfQuadrati(final int a, final int b, final int c) {
         final int i = 4, j = 2;
