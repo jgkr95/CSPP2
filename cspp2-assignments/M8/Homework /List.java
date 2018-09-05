a02e6e03b401f37bd8c2af7be2bc0bd86a9598bf
jgkr95 <jgkr95@msitprogram.net>
2018-09-05 22:35:33 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2-assignments/M8/Homework /List.java
+++ cspp2-assignments/M8/Homework /List.java
@@ -328,7 +328,8 @@ public class List {
                         l.add(Integer.parseInt(tokens[1]));
                     } else {
                         if (t.length > 1)
-                            l.add(Integer.parseInt(t[0]), Integer.parseInt(t[1]));
+                            l.add(Integer.parseInt(t[0])
+                                , Integer.parseInt(t[1]));
                     }
                 }
                 break;
@@ -338,9 +339,10 @@ public class List {
             case "addAll":
                 if (tokens.length == 2) {
                     String[] t1 = tokens[1].split(",");
-                    int temp[] = new int[t1.length];
-                    for (int i = 0; i < temp.length; i++)
+                    int[] temp = new int[t1.length];
+                    for (int i = 0; i < temp.length; i++) {
                         temp[i] = Integer.parseInt(t1[i]);
+                    }
                     l.addAll(temp);
                 }
                 break;
