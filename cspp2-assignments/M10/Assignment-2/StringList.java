0ac52ffd87b552e43d2705e9d24b22f9beb1a236
jgkr95 <jgkr95@msitprogram.net>
2018-09-06 10:10:42 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2-assignments/M10/Assignment-2/StringList.java
+++ cspp2-assignments/M10/Assignment-2/StringList.java
@@ -171,10 +171,14 @@ public class StringList implements StringListInterface {
     public void remove(int index) {
         // write the logic for remove here.
         // Think about what to do to the size variable.
-        for (int i = index; i < size-1; i++) {
-            list[i] = list[i + 1];
+        if(index>-1&&index<size) {
+            for (int i = index; i < size-1; i++) {
+                list[i] = list[i + 1];
+            }
+            size--;
+        } else {
+            System.out.println("Invalid Position Exception");
         }
-        size--;
 
     }
 
