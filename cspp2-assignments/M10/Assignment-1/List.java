9bdb67ef0d4424043e267c8667cb389a47c89a5c
jgkr95 <jgkr95@msitprogram.net>
2018-09-06 18:04:48 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2-assignments/M10/Assignment-1/List.java
+++ cspp2-assignments/M10/Assignment-1/List.java
@@ -284,6 +284,8 @@ final class List {
      * @return     String representation of the list.
      */
     public String toString() {
+        if (size == 0)
+            return "[]";
         String str = "[";
         int i = 0;
         for (i = 0; i < size - 1; i++) {
