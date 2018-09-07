6c44547e01996a06f3eac8441831e805f799c252
jgkr95 <jgkr95@msitprogram.net>
2018-09-07 13:53:31 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/M11/Assignment-1/List.java
+++ cspp2-assignments/M11/Assignment-1/List.java
@@ -223,7 +223,7 @@ public class List {
     public void removeAll(int[] newArray) {
         // write the logic
         int indexx;
-        for (int i:list) {
+        for (int i=0;i<size;i++) {
             for (int j = 0; j < newArray.length; j++) {
                 // if (contains(newArray[j])) {
                 //  int index = indexOf(newArray[j]);
