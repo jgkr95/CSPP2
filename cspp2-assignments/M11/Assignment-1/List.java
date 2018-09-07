768fc21718068cab3b70650550458908ab5a5a33
jgkr95 <jgkr95@msitprogram.net>
2018-09-07 19:54:56 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cspp2-assignments/M11/Assignment-1/List.java
+++ cspp2-assignments/M11/Assignment-1/List.java
@@ -474,7 +474,7 @@ public class List {
                     l.removeAll(a);
                 }
                 break;
-            case "subList": {
+            case "subList": 
                 if (tokens.length != 2) {
                     break;
                 }
@@ -485,7 +485,6 @@ public class List {
                     System.out.println(object);
                 }
                 break;
-            }
             case "equals":
                 if (tokens.length == 2) {
                     String[] lt = tokens[1].split(",");
