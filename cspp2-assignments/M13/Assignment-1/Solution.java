177cd00937dd5342be94e64f626ee33614f5ca89
jgkr95 <jgkr95@msitprogram.net>
2018-09-08 11:09:40 +0530

    "check_style_score -> 1 of 4 passed. style score: 0/1 "

--- cspp2-assignments/M13/Assignment-1/Solution.java
+++ cspp2-assignments/M13/Assignment-1/Solution.java
@@ -181,7 +181,7 @@ public final class Solution {
 				s.add(intArray);
 				intArray = intArray(tokens[2]);
 				t.add(intArray);
-				// System.out.println(Arrays.deepToString(s.cartesianProduct(t)));
+				System.out.println(Arrays.deepToString(s.cartesianProduct(t)));
 				break;
 			default:
 				break;
