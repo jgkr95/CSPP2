aa3fc6496bc6585eb2291c3c779c346fc55d5534
jgkr95 <jgkr95@msitprogram.net>
2018-09-10 21:23:26 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

--- cspp2-assignments/M14/SortedSet/Solution.java
+++ cspp2-assignments/M14/SortedSet/Solution.java
@@ -229,7 +229,7 @@ class SortedSet extends Set {
 		// System.out.println(Arrays.toString(arr));
 		Arrays.sort(arr);
 		// System.out.println(Arrays.toString(arr));
-		// System.out.println(Arrays.toString(arr));
+		System.out.println(Arrays.toString(arr));
 		for (int i = 0; i < arr.length; i++) {
 			// System.out.println(arr[i]);
 			if (!contains(arr[i])) {
