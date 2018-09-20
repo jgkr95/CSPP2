8ddda57064fa58e242adf6b9073ad1f65402128e
jgkr95 <jgkr95@msitprogram.net>
2018-09-20 18:48:51 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

---
+++ cspp2-assignments/M23/Assignment-1/Solution.java
@@ -0,0 +1,100 @@
+import java.io.*;
+import java.io.BufferedReader;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.HashMap;
+class Solution {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+
+		// File file1 = new File("lewis.txt");
+		// File file2 = new File("verne.txt");
+		// Scanner f1 = new Scanner(new FileReader(file1));
+		// Scanner f2 = new Scanner(new FileReader(file2));
+		String dirPath = sc.next();
+		File dir = new File(dirPath);
+		String[] files = dir.list();
+		int[][] cos = new int[files.length][files.length];
+		// System.out.println(Arrays.toString(files));
+		// System.out.println(files[0]);
+		if (files.length == 0) {
+		    System.out.println("empty directory");
+		} else {
+		    for (int i=0;i<files.length;i++) {
+		    	for(int j=0;j<files.length;j++) {
+		        // System.out.println(aFile);
+			        HashMap<String, Integer> firstfile = readfile(new Scanner(new FileReader(dir+"//"+files[i])));
+					HashMap<String, Integer> secondfile = readfile(new Scanner(new FileReader(dir+"//"+files[j])));
+					// System.out.println(Arrays.asList(firstfile));
+					// System.out.println(Arrays.asList(secondfile));
+					Integer[] firstkeyvalues = firstfile.values().toArray(new Integer[0]);
+					Integer[] secondkeyvalues = secondfile.values().toArray(new Integer[0]);
+					double euclidnorm1 = Euclideannorm(firstkeyvalues), euclidnorm2 = Euclideannorm(secondkeyvalues);
+					double dotpro = dotproduct(firstfile, secondfile);
+					cos[i][j] = (int)(Cosinesimilarity(euclidnorm1,euclidnorm2,dotpro)*10000)/100;
+					
+				}
+		    }
+		}
+		// System.out.println(Arrays.deepToString(cos));
+		int max = 0;
+		int index1 = 100, index2 = 100;
+		for(int i=0;i<files.length;i++) {
+			System.out.print("  \t"+files[i]);
+		}System.out.println();
+		for(int i=0;i<files.length;i++) {
+			System.out.print(files[i]+"\t");
+			for(int j=0;j<files.length;j++) {
+				System.out.print(cos[i][j]+"\t");
+				if(cos[i][j]<100&&cos[i][j]>max) {
+					max = cos[i][j];
+					index1 = i;
+					index2 = j;
+				}
+			}System.out.println();
+		}
+		// System.out.println("Maximum similarity is between "+ +" and ");
+	}
+	public static double Euclideannorm(Integer[] euc) {
+		double mod=0,euclid=0;
+		for(int i=0;i<euc.length;i++) {
+			mod += Math.pow(euc[i],2);
+		}
+		euclid = Math.sqrt(mod);
+		return euclid;
+	}
+	public static double dotproduct(HashMap<String, Integer> ffile1, HashMap<String, Integer> ffile2) {
+		String[] file1keys = ffile1.keySet().toArray(new String[0]);
+		int similarity=0;
+		for(int i=0;i<file1keys.length;i++) {
+			if(ffile2.containsKey(file1keys[i])) {
+				similarity += ffile1.get(file1keys[i])*ffile2.get(file1keys[i]);
+			}
+		}
+		return similarity;
+
+	}
+	public static double Cosinesimilarity(double numerator1, double numerator2, double denominator) {
+		double cosine = (numerator1 * numerator2)/denominator;
+		return cosine;
+
+	}
+	public static HashMap<String, Integer> readfile(Scanner s) {
+		HashMap<String, Integer> wordsinfile = new HashMap<String, Integer>();
+		while (s.hasNext()) {
+			String line = s.nextLine().toLowerCase();
+			line = line.replaceAll("[0-9_]", "");
+			String[] words = line.split(" ");
+			for (int i = 0; i < words.length && words[i].length() > 0; i++) {
+				// System.out.println("_"+words[i]+"_");
+				if (wordsinfile.containsKey(words[i].replace(" ",""))) {
+					wordsinfile.put(words[i].replace(" ",""), wordsinfile.get(words[i]) + 1);
+				} else {
+					wordsinfile.put(words[i].replace(" ",""), 1);
+				}
+			}
+		}
+		// System.out.println("===============");
+		return wordsinfile;
+	}
+}
