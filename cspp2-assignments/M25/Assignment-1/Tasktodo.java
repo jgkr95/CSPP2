51fdbe6fda5ca6dadd6c3d5c4cc575c93be52857
jgkr95 <jgkr95@msitprogram.net>
2018-09-22 11:38:00 +0530

    "check_style_score -> 3 of 14 passed. style score: 0/1 "

---
+++ cspp2-assignments/M25/Assignment-1/Tasktodo.java
@@ -0,0 +1,94 @@
+class Task {
+	private String tasktitle;
+	private String taskowner;
+	private int timeperiod;
+	private boolean importantornot;
+	private boolean urgentornot;
+	private String progress;
+
+	Task(String tasktitle1, String taskowner1, int timeperiod1,
+	 boolean importantornot1, boolean urgentornot1, String progress1) throws Exception{
+		if(!(tasktitle1.length()>0)) {
+			throw new Exception("Title not provided");
+		}
+		if(timeperiod1<0) {
+			throw new Exception("Invalid timeToComplete "+timeperiod1);
+		}
+		if(!(progress1.equals("todo")||progress1.equals("done"))) {
+			throw new Exception("Invalid status");
+		}
+		tasktitle=tasktitle1;
+		taskowner=taskowner1;
+		timeperiod=timeperiod1;
+		importantornot=importantornot1;
+		urgentornot=urgentornot1;
+		progress=progress1;
+	}
+	public String gettaskowner() {
+		return taskowner;
+	}
+	public boolean getimportant() {
+		return importantornot;
+	}
+	public boolean geturgent() {
+		return urgentornot;
+	}
+	public String getprogress() {
+		return progress;
+	}
+	public int gettimeperiod() {
+		return timeperiod;
+	}
+	public String toString() {
+		String str = tasktitle + ", "+taskowner+", "+timeperiod+", "+importantornot+", "+urgentornot+", "+progress;
+		return str;
+	}
+} 
+class Todoist {
+	Task[] taskobj;
+	int size;
+	final int twenty = 20;
+	Todoist() {
+		taskobj = new Task[twenty];
+		size = 0;
+	}
+	public void addTask(Task t) {
+		taskobj[size++] = t;
+	}
+	public String toString() {
+		return taskobj[size-1].toString();
+	}
+	public Task getNextTask(String name) {
+		for(int i=0;i<size;i++) {
+			if(taskobj[i].gettaskowner().equals(name)&&taskobj[i].getimportant()&&(!(taskobj[i].geturgent()))&&taskobj[i].getprogress().equals("todo")) {
+				return taskobj[i];
+			} else if(taskobj[i].gettaskowner().equals(name)&&taskobj[i].getimportant()&&taskobj[i].geturgent()&&taskobj[i].getprogress().equals("todo")) {
+				return taskobj[i];
+			}
+			
+		}
+		return null;
+
+	}
+	public Task[] getNextTask(String name, int count) {
+		Task[] nexttasks = new Task[count];
+		int sizenexttask = 0;
+		for(int i=0;i<size;i++) {
+			if(taskobj[i].gettaskowner().equals(name)&&taskobj[i].getimportant()&&(!(taskobj[i].geturgent()))&&taskobj[i].getprogress().equals("todo")) {
+				nexttasks[sizenexttask++]=taskobj[i];
+			} else if(taskobj[i].gettaskowner().equals(name)&&taskobj[i].getimportant()&&taskobj[i].geturgent()&&taskobj[i].getprogress().equals("todo")) {
+				nexttasks[sizenexttask++]=taskobj[i];
+			}
+		}
+		return nexttasks;
+
+	}
+	public int totalTime4Completion() {
+		int time=0;
+		for(int i=0;i<size;i++) {
+			time += taskobj[i].gettimeperiod();
+		}
+		return time;
+	}
+
+}
