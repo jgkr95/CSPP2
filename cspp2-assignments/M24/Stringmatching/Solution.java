8eed87ae31ca136dd7441893a294eb9f786b1cd1
jgkr95 <jgkr95@msitprogram.net>
2018-09-23 17:38:05 +0530

    "check_style_score -> 1 of 3 passed. style score: 1/1 "

--- cspp2-assignments/M24/Stringmatching/Solution.java
+++ cspp2-assignments/M24/Stringmatching/Solution.java
@@ -92,9 +92,9 @@ public final class Solution {
         String res = "";
         double[][] fileM = new double[len][len];
         File tmp = null;
-        for(int i = 0; i < list.length; i++) {
-            for(int j = i + 1; j < list.length; j++) {
-                if(list[i].getName().compareTo(list[j].getName())>0) {
+        for (int i = 0; i < list.length; i++) {
+            for (int j = i + 1; j < list.length; j++) {
+                if (list[i].getName().compareTo(list[j].getName()) > 0) {
                     tmp = list[i];
                     list[i] = list[j];
                     list[j] = tmp;
